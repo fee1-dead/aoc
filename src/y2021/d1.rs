cc9c9ed25d491151dddd5500d4e86628266e5216
Deadbeef <ent3rm4n@gmail.com>
2021-12-01 15:25:01 +0800

    separate lines

--- src/y2021/d1.rs
+++ src/y2021/d1.rs
@@ -3,7 +3,9 @@ use crate::*;
 pub fn part1(s: String) -> Result<()> {
     let integers: Vec<usize> = s.lines().map(str::parse).collect::<Result<_, _>>()?;
     let inc = integers.windows(2).filter(|v| v[1] > v[0]).count();
+    
     dbg!(inc);
+
     Ok(())
 }
 
@@ -13,6 +15,8 @@ pub fn part2(s: String) -> Result<()> {
         .windows(4)
         .filter(|v| v[1..].iter().copied().sum::<usize>() > v[..3].iter().copied().sum())
         .count();
+
     dbg!(inc);
+
     Ok(())
 }
