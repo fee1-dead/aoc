f94e4a0a2900052da7218ec7f42f5e2c705289da
Deadbeef <ent3rm4n@gmail.com>
2021-12-01 17:06:38 +0800

    2015 Day 10

---
+++ src/y2015/d10.rs
@@ -0,0 +1,50 @@
+use crate::*;
+
+pub fn look_and_say(s: &str) -> String {
+    let mut last_char = s.chars().next().unwrap();
+    let mut last_count = b'0';
+    let mut st = String::new();
+
+    for ch in s.chars() {
+        if last_char != ch {
+            st.push(last_count as char);
+            st.push(last_char);
+
+            last_char = ch;
+            last_count = b'1';
+        } else {
+            last_count += 1;
+        }
+    }
+
+    st.push(last_count as char);
+    st.push(last_char);
+
+    st
+}
+
+pub fn part1(mut s: String) -> Result<()> {
+    let mut input = s.trim();
+
+    for _ in 0..40 {
+        s = look_and_say(input);
+        input = &s;
+    }
+
+    dbg!(s.len());
+
+    Ok(())
+}
+
+pub fn part2(mut s: String) -> Result<()> {
+    let mut input = s.trim();
+
+    for _ in 0..50 {
+        s = look_and_say(input);
+        input = &s;
+    }
+
+    dbg!(s.len());
+
+    Ok(())
+}
